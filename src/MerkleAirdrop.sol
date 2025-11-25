9a2b14979f6396708ab53a521e1dd708c1ba4a93
Contzo <ilierazvan1497@outlook.com>
2025-11-25 16:17:11 +0200

    dfsdaf

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,10 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 /// @title MerkleAirDrop
 /// @author Contzo
 contract MerkleAirDrop {
     // List of allowed addresses to claim tokens
     // Allow someone in the list to claim tokens
-    address[] public claimableAddresses;
-}
+    address[] public claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 public i_airdropToken;
+
+    constrcutor(bytes32 merkleRoot, IERC20 airdropToken) {
+      i_merkleRoot = merkleRoot;
+      i_airdropToken = airdropToken;
+      
+    }
+  }
